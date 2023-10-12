726668077bb1a7dd2915b277c6c5285806f0dbb8
graphFatigue <foxmary007@gmail.com>
2023-10-12 14:33:15 +0300

    release v2

--- wpf_attempt2/obj/Debug/net6.0-windows/SinglePage.g.i.cs
+++ wpf_attempt2/obj/Debug/net6.0-windows/SinglePage.g.i.cs
@@ -77,7 +77,7 @@ namespace wpf_attempt2 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/wpf_attempt2;V1.0.0.0;component/singlepage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/wpf_attempt2;component/singlepage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\SinglePage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
